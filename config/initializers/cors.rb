32413b99a2e6b8f71be8282594afe0a040924a2c
prasad3543u <devavaraprasadreddy123@gmail.com>
2026-03-18 15:34:00 +0530

    Fix CORS configuration syntax error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch('https://cinecrick-spa.vercel.app', 'http://localhost:5173')
+    origins 'https://cinecrick-spa.vercel.app', 'http://localhost:5173'
+    
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
